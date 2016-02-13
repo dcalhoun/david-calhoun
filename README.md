2617e87a3c63410156beb480878ac0e075682189
David Calhoun <dpcalhoun@gmail.com>
2016-02-13 16:47:05 -0600

    Add post content files

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Source for David Calhoun's personal site.
 
 ## TODO
-- Figure out how to pass props down to children components through react-router
+- Generate posts from Markdown files
+- Add style
--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  byline: 'David Calhoun is a designer/developer specializing in front-end architecture and UX design. He is experienced with Ember.js, React.js, Backbone.js, and Ruby on Rails. He is currently a Senior Engineer at GoNoodle in Nashville, TN.',
+  byline: 'David Calhoun is a designer/developer specializing in front-end architecture and UX design. He is experienced with Ember.js, React.js, Backbone.js, and Ruby on Rails. He is currently a Senior Front-End Engineer at GoNoodle in Nashville, TN.',
   title: 'David Calhoun, Design & Developer',
   paths: [
     '/',
