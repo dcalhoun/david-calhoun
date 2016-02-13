57891d0aef7f259b2da5914f5e5accc8a171f445
David Calhoun <dpcalhoun@gmail.com>
2016-02-13 16:24:17 -0600

    Update webpack config to match dev

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var calc          = require('postcss-calc');
 var customMedia   = require('postcss-custom-media');
 var customProps   = require('postcss-custom-properties');
 var locals        = require('./src/data.js');
+var path          = require('path');
 var postcssImport = require('postcss-import');
 var StaticSite    = require('static-site-generator-webpack-plugin');
 var webpack       = require('webpack');
@@ -13,7 +14,7 @@ module.exports = {
 
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/dist',
+    path: path.join(__dirname, '/dist'),
     libraryTarget: 'umd'
   },
 
