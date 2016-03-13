59d479c92c9c984043f9eeadba8f61f007a114ea
David Calhoun <dpcalhoun@gmail.com>
2016-03-13 16:48:47 -0500

    Clean up

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # DavidCalhoun.me
 
 Source for David Calhoun's personal site.
-
-## TODO
-- Pass Markdown file props to child routes
-- Generate posts from Markdown files
-- Add style
--- src/data.js
+++ src/data.js
@@ -24,7 +24,7 @@ const paths = {
 module.exports = {
   byline: 'David Calhoun is a designer/developer specializing in front-end architecture and UX \
           design. He is experienced with Ember.js, React.js, Backbone.js, and Ruby on Rails. He is \
-          currently a Senior Front-End Engineer at GoNoodle in Nashville, TN.',
+          currently a Senior Engineer at GoNoodle in Nashville, TN.',
   paths: paths,
   routes: routes,
   title: 'David Calhoun, Design & Developer',
