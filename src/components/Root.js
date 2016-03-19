680c8a4e00cd9a4447afe83e1581d72b396eec9e
David Calhoun <dpcalhoun@gmail.com>
2016-03-18 22:10:01 -0500

    Repair broken Root component

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,24 +1,30 @@
 import css from '../app.css';
 import React from 'react';
 
-const Root = (assets, children, title, ...props) => (
-  <html>
-    <head>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='width=device-width,initial-scale=1' />
-      <title>{title}</title>
-      <link rel='shortcut icon' type='image/png' href='/favicon.png' />
-      <link rel='stylesheet' href='/main.css' />
-    </head>
-    <body>
-      <div id='js-outlet'>
-        {children}
-      </div>
-      {Object.keys(assets).map((chunk, index) => {
-        return <script key={index} src={'/' + assets[chunk]} />;
-      })}
-    </body>
-  </html>
-);
+const Root = React.createClass({
+  render() {
+    const { assets, children, title } = this.props;
+
+    return (
+      <html>
+        <head>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='width=device-width,initial-scale=1' />
+          <title>{title}</title>
+          <link rel='shortcut icon' type='image/png' href='/favicon.png' />
+          <link rel='stylesheet' href='/main.css' />
+        </head>
+        <body>
+          <div id='js-outlet'>
+            {children}
+          </div>
+          {Object.keys(assets).map((chunk, index) => {
+            return <script key={index} src={'/' + assets[chunk]} />;
+          })}
+        </body>
+      </html>
+    );
+  }
+});
 
 export default Root;
