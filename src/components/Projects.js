c3e0cfab5af0b97ae6ba6230746987ffe3dc7d8c
David Calhoun <dpcalhoun@gmail.com>
2016-04-02 15:33:44 -0500

    Fix missing props

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,12 +9,10 @@ const Projects = React.createClass({
       <section className='flex flex-wrap mxn2'>
         <h2 className='h6 caps my2 px2 col-12'>Open Source</h2>
         <ProjectCard
-          { ...props }
           name='Playbook'
           description='A Yeoman generator for prototyping and building experiences.'
           href='http://playbook.centresource.com'/>
         <ProjectCard
-          { ...props }
           name='Alfred Remote Inspector'
           description='Remotely inspect any iOS device by name via Safari’s web inspector.'
           href='https://github.com/dcalhoun/alfred-remote-inspector-workflow'/>
