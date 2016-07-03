2d9746dcf567eeb4e9068b32e344eae0af496b85
David Calhoun <dpcalhoun@gmail.com>
2016-07-03 14:27:39 -0500

    Update site content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,13 +9,21 @@ const Projects = React.createClass({
       <section className='flex flex-wrap mxn2'>
         <h2 className='h6 caps my2 px2 col-12'>Open Source</h2>
         <ProjectCard
-          name='Playbook'
-          description='A Yeoman generator for prototyping and building experiences.'
-          href='http://playbook.centresource.com'/>
-        <ProjectCard
           name='Alfred Remote Inspector'
           description='Remotely inspect any iOS device by name via Safari’s web inspector.'
           href='https://github.com/dcalhoun/alfred-remote-inspector-workflow'/>
+        <ProjectCard
+          name='css-utils-margin'
+          description='Immutable, CSS margin utilities.'
+          href='http://github.com/dcalhoun/css-utils-margin'/>
+        <ProjectCard
+          name='css-utils-padding'
+          description='Immutable, CSS padding utilities.'
+          href='http://github.com/dcalhoun/css-utils-padding'/>
+        <ProjectCard
+          name='Playbook'
+          description='A Yeoman generator for prototyping and building experiences.'
+          href='http://playbook.centresource.com'/>
       </section>
     )
   }
