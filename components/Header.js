13e6fc643f7f019089c13b5178c7dc98f3c61dcb
David Calhoun <dpcalhoun@gmail.com>
2017-04-29 17:07:45 -0500

    Add styles to Header

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,62 @@
 import Link from 'next/link'
+import connectStyles from 'styletron-connect'
 
-const Header = () => (
-  <header className='flex flex-wrap py3'>
-    <h1 className='h2 m0 mr1 inline-block'>
+const Header = ({ styles }) => (
+  <header className={styles.header}>
+    <h1 className={styles.title}>
       <Link href='/'>
-        <a>David Calhoun</a>
+        <a className={styles.anchor}>David Calhoun</a>
       </Link>
     </h1>
-    <div className='flex-auto' />
-    <nav className='flex flex-baseline mxn1'>
+    <div className={styles.spacer} />
+    <nav className={styles.nav}>
       <Link href='/writing'>
-        <a className='h6 btn p1'>Writing</a>
+        <a className={styles.link}>Writing</a>
       </Link>
-      <a className='h6 btn p1' href='http://twitter.com/david_calhoun'>
+      <a
+        className={styles.link}
+        href='http://twitter.com/david_calhoun'
+      >
         Twitter
       </a>
-      <a className='h6 btn p1' href='http://github.com/dcalhoun'>
+      <a
+        className={styles.link}
+        href='http://github.com/dcalhoun'
+      >
         GitHub
       </a>
     </nav>
   </header>
 )
 
-export default Header
+export default connectStyles(Header, {
+  header: {
+    display: 'flex',
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Helvetica, sans-serif',
+    flexWrap: 'flex-wrap',
+    padding: '2rem'
+  },
+  title: {
+    display: 'inline-block',
+    fontSize: '1.5rem',
+    fontWeight: '600',
+    margin: '0 1rem 0 0'
+  },
+  anchor: {
+    color: '#555',
+    textDecoration: 'none'
+  },
+  spacer: {
+    flex: '1 1 auto'
+  },
+  nav: {
+    display: 'flex',
+    margin: '0 -0.5rem'
+  },
+  link: {
+    color: '#555',
+    fontSize: '0.75rem',
+    padding: '0.5rem',
+    textDecoration: 'none'
+  }
+})
