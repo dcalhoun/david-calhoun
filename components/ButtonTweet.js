2a4496b75e5768851d15b2aa558e6be921e1e223
David Calhoun <dpcalhoun@gmail.com>
2017-08-05 15:34:02 -0500

    Fix Tweet button loading

--- components/ButtonTweet.js
+++ components/ButtonTweet.js
@@ -1,27 +1,50 @@
-import Head from 'next/head'
+import { Component } from 'react'
 import { styled } from 'styletron-react'
 
-const ButtonTweet = styled('nav', {
+const Anchor = styled('nav', {
   display: 'inline-block',
   marginBottom: '1.5em'
 })
 
-export default ({ title }) => {
-  const twitterScript = 'window.twttr=function(t,e,r){var n,i=t.getElementsByTagName(e)[0],w=window.twttr||{};return t.getElementById(r)?w:(n=t.createElement(e),n.id=r,n.src="https://platform.twitter.com/widgets.js",i.parentNode.insertBefore(n,i),w._e=[],w.ready=function(t){w._e.push(t)},w)}(document,"script","twitter-wjs");'
+class ButtonTweet extends Component {
+  constructor (props) {
+    super(props)
+    this.widget = this.loadTwitterWidget()
+  }
 
-  return (
-    <ButtonTweet>
-      <Head>
-        <script dangerouslySetInnerHTML={{ __html: twitterScript }} />
-      </Head>
-      <a href='https://twitter.com/share'
-        className='twitter-share-button'
-        data-size='large'
-        data-text={title}
-        data-via='david_calhoun'
-      >
-        Tweet
-      </a>
-    </ButtonTweet>
-  )
+  loadTwitterWidget () {
+    return new Promise((resolve, reject) => {
+      const firstScript = document.getElementsByTagName('script')[0]
+      const twttrScript = document.createElement('script')
+      twttrScript.id = 'twitter-wjs'
+      twttrScript.addEventListener('load', () => { resolve() })
+      twttrScript.src = 'https://platform.twitter.com/widgets.js'
+      firstScript.parentNode.insertBefore(twttrScript, firstScript)
+    })
+  }
+
+  componentDidMount () {
+    this.widget.then(() => {
+      window.twttr.widgets.createShareButton(
+        window.location.href,
+        this.anchor,
+        {
+          size: 'large',
+          text: this.props.title,
+          via: 'david_calhoun'
+        }
+      )
+    })
+  }
+
+  // TODO: Move `ref` to `Anchor` when styletron@3.x.x is out.
+  render () {
+    return (
+      <Anchor>
+        <a ref={anchor => { this.anchor = anchor }} />
+      </Anchor>
+    )
+  }
 }
+
+export default ButtonTweet
