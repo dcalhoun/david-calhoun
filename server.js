0f48bed692038410c998dc143f69dcd794e01a6b
David Calhoun <dpcalhoun@gmail.com>
2017-08-09 18:33:57 -0500

    Add ability to pass server port

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const args = {}
+process.argv
+  .slice(2, process.argv.length)
+  .map(x => x.substring(2))
+  .forEach(x => {
+    const sep = x.indexOf('=')
+    args[x.slice(0, sep)] = x.substring(sep + 1)
+  })
+
 app.prepare()
 .then(() => {
   createServer(async (req, res) => {
@@ -19,8 +28,8 @@ app.prepare()
       handle(req, res, parsedUrl)
     }
   })
-  .listen(3000, (err) => {
+  .listen(args.port || 3000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${args.port || 3000}`)
   })
 })
