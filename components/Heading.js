e71ed8bbdc9ef995856fea9a19ffa79d4a3d7ea9
David Calhoun <dpcalhoun@gmail.com>
2018-04-15 08:50:56 -0500

    [add] Yarn and EB post

--- components/Heading.js
+++ components/Heading.js
@@ -1,38 +1,37 @@
-import PropTypes from 'prop-types'
-import { Component } from 'react'
-import { injectStyle } from 'styletron-utils'
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { injectStyle } from 'styletron-utils';
 
 class Heading extends Component {
-  classNames () {
+  classNames(level) {
     // TODO: Why does a normal `styled` component's classes get
     //       lost on the way into `react-markdown`?
-    return injectStyle(
-      this.context.styletron,
-      {
-        fontSize: '1.125rem',
-        marginBottom: '0.5em',
-        marginTop: '1.75em',
+    return injectStyle(this.context.styletron, {
+      fontSize: level < 4 ? '1.125rem' : '0.875rem',
+      fontWeight: level < 4 ? 'bold' : 'normal',
+      marginBottom: '0.5em',
+      marginTop: '1.75em',
+      textTransform: level < 4 ? 'none' : 'uppercase',
 
-        '@media (min-width: 40rem)': {
-          fontSize: '1.5rem'
-        }
-      }
-    )
+      '@media (min-width: 40rem)': {
+        fontSize: level < 4 ? '1.5rem' : '1rem',
+      },
+    });
   }
 
-  render () {
-    const TagName = `h${this.props.level}`
+  render() {
+    const TagName = `h${this.props.level}`;
 
     return (
-      <TagName className={this.classNames()}>
+      <TagName className={this.classNames(this.props.level)}>
         {this.props.children}
       </TagName>
-    )
+    );
   }
 }
 
 Heading.contextTypes = {
-  styletron: PropTypes.object
-}
+  styletron: PropTypes.object,
+};
 
-export default Heading
+export default Heading;
