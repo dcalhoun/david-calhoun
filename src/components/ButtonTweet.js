fad5e9907553127489801c8b41d24c203cf1a303
David Calhoun <dpcalhoun@gmail.com>
2019-03-23 18:11:28 -0500

    [change] Follow hooks recommendations

--- src/components/ButtonTweet.js
+++ src/components/ButtonTweet.js
@@ -3,38 +3,38 @@ import React, { useLayoutEffect, useRef } from "react";
 import { Component } from "react";
 import { styled } from "styletron-react";
 
-async function initTweetButton(anchor, text) {
-  if (typeof window !== "undefined") {
-    if (typeof window.twitter === "undefined") {
-      try {
-        await new Promise(resolve => {
-          const firstScript = document.getElementsByTagName("script")[0];
-          const twttrScript = document.createElement("script");
-          twttrScript.id = "twitter-wjs";
-          twttrScript.addEventListener("load", resolve);
-          twttrScript.src = "//platform.twitter.com/widgets.js";
-          firstScript.parentNode.insertBefore(twttrScript, firstScript);
-        });
-      } catch (e) {
-        console.error("Twitter load failed.", e);
-      }
-    }
-
-    try {
-      await window.twttr.widgets.createShareButton(
-        window.location.href,
-        anchor.current,
-        { size: "large", text, via: "david_calhoun" }
-      );
-    } catch (e) {
-      console.error("Twitter initialization failed.", e);
-    }
-  }
-}
-
 function ButtonTweet(props) {
   const anchor = useRef(null);
   useLayoutEffect(() => {
+    async function initTweetButton(anchor, text) {
+      if (typeof window !== "undefined") {
+        if (typeof window.twitter === "undefined") {
+          try {
+            await new Promise(resolve => {
+              const firstScript = document.getElementsByTagName("script")[0];
+              const twttrScript = document.createElement("script");
+              twttrScript.id = "twitter-wjs";
+              twttrScript.addEventListener("load", resolve);
+              twttrScript.src = "//platform.twitter.com/widgets.js";
+              firstScript.parentNode.insertBefore(twttrScript, firstScript);
+            });
+          } catch (e) {
+            console.error("Twitter load failed.", e);
+          }
+        }
+
+        try {
+          await window.twttr.widgets.createShareButton(
+            window.location.href,
+            anchor.current,
+            { size: "large", text, via: "david_calhoun" }
+          );
+        } catch (e) {
+          console.error("Twitter initialization failed.", e);
+        }
+      }
+    }
+
     initTweetButton(anchor, props.title);
   }, [anchor, props.title]);
 
