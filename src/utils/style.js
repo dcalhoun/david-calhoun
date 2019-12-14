348ae187d149a0ffeac8e35b08736661fe91cb4f
David Calhoun <dpcalhoun@gmail.com>
2019-12-14 15:34:35 -0600

    Remove current source

--- src/utils/style.js
+++
@@ -1,9 +0,0 @@
-const base =
-  '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Helvetica, sans-serif';
-
-export const fonts = {
-  body: `"Avenir Next", ${base}`,
-  base,
-  heading: `"Avenir Next", ${base}`,
-  monospace: "Monaco, monospace"
-};
