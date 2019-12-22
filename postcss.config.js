9f27f1b06dd11b55e0015698c5827dd53cb54a89
David Calhoun <dpcalhoun@gmail.com>
2019-12-22 13:50:21 -0600

    Update purgecss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,8 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("@fullhuman/postcss-purgecss")({
-      content: ["./pages/**/*.js", "./components/**/*.js"]
+      content: ["./pages/**/*.js", "./components/**/*.js"],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     }),
     require("autoprefixer"),
     require("cssnano")
