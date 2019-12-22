d050bede6501b1bee2f624a5856b5d9f25c5310f
David Calhoun <dpcalhoun@gmail.com>
2019-12-22 09:22:39 -0600

    Avoid duplicative head tags

--- components/SEO.js
+++ components/SEO.js
@@ -14,18 +14,26 @@ export default ({
   url
 }) => (
   <Head>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-    <meta name="og:description" content={description} />
-    <meta name="og:image:height" content={imageHeight} />
-    <meta name="og:image:width" content={imageWidth} />
-    <meta name="og:image" content={`${host}${image}`} />
-    <meta name="og:title" content={title} />
-    <meta name="og:type" content="website" />
-    <meta name="og:url" content={url} />
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:creator" content="site.siteMetadata.author" />
-    <meta name="twitter:description" content="metaDescription" />
-    <meta name="twitter:title" content="title" />
+    <title key="title">{title}</title>
+    <meta key="description" name="description" content={description} />
+    <meta key="og:description" name="og:description" content={description} />
+    <meta key="og:image:height" name="og:image:height" content={imageHeight} />
+    <meta key="og:image:width" name="og:image:width" content={imageWidth} />
+    <meta key="og:image" name="og:image" content={`${host}${image}`} />
+    <meta key="og:title" name="og:title" content={title} />
+    <meta key="og:type" name="og:type" content="website" />
+    <meta key="og:url" name="og:url" content={url} />
+    <meta key="twitter:card" name="twitter:card" content="summary" />
+    <meta
+      key="twitter:creator"
+      name="twitter:creator"
+      content="site.siteMetadata.author"
+    />
+    <meta
+      key="twitter:description"
+      name="twitter:description"
+      content="metaDescription"
+    />
+    <meta key="twitter:title" name="twitter:title" content="title" />
   </Head>
 );
