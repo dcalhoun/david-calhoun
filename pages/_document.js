3987a88b4a3fe35bf5f142da0c8b9db78a21becf
David Calhoun <dpcalhoun@gmail.com>
2019-12-26 12:25:45 -0600

    Manually add icon assets
    Ideally the icon is generated from a single asset at build time. Need to
    identify an approach that suits Next.js.

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,39 @@ class MyDocument extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="/icon-48x48.png" />
+          <link rel="manifest" href="/manifest.webmanifest" />
+          <meta name="theme-color" content="#4e1fe0" />
+          <link rel="apple-touch-icon" sizes="48x48" href="/icon-48x48.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="96x96" href="/icon-96x96.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="/icon-192x192.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="256x256"
+            href="/icon-256x256.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="384x384"
+            href="/icon-384x384.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="512x512"
+            href="/icon-512x512.png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
