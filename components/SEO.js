2a58e6b48c37016f0c76a68dbec4aa8315be4981
David Calhoun <dpcalhoun@gmail.com>
2019-12-26 12:55:58 -0600

    Fix issues with Open Graph tags
    Ideally the origin would be detected at build time, rather than being a
    static value.

--- components/SEO.js
+++ components/SEO.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 const SITE_TITLE = "David Calhoun, Software Engineer";
 export const SITE_DESCRIPTION =
   "David Calhoun is a software engineer with a passion for good UX design & scalable architecture.";
-const HOST = "davidcalhoun.me";
+const ORIGIN = "https://davidcalhoun.me";
 
 function generateTitle(pageTitle) {
   return pageTitle ? `${pageTitle} | ${SITE_TITLE}` : SITE_TITLE;
@@ -30,23 +30,31 @@ export default function SEO({
       />
       <meta
         key="og:description"
-        name="og:description"
+        property="og:description"
         content={description || SITE_DESCRIPTION}
       />
       <meta
         key="og:image:height"
-        name="og:image:height"
-        content={imageHeight}
+        property="og:image:height"
+        content={imageHeight || 630}
       />
-      <meta key="og:image:width" name="og:image:width" content={imageWidth} />
+      <meta
+        key="og:image:width"
+        property="og:image:width"
+        content={imageWidth || 1200}
+      />
       <meta
         key="og:image"
-        name="og:image"
-        content={image || `//${HOST}/david.jpg`}
+        property="og:image"
+        content={image || `${ORIGIN}/david.jpg`}
       />
-      <meta key="og:title" name="og:title" content={generateTitle(title)} />
-      <meta key="og:type" name="og:type" content="website" />
-      <meta key="og:url" name="og:url" content={`//${HOST}${router.route}`} />
+      <meta key="og:title" property="og:title" content={generateTitle(title)} />
+      <meta key="og:type" property="og:type" content="website" />
+      <meta
+        key="og:url"
+        property="og:url"
+        content={`${ORIGIN}${router.route}`}
+      />
       <meta key="twitter:card" name="twitter:card" content="summary" />
       <meta
         key="twitter:creator"
