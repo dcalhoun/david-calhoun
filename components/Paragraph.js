02f9b8dcf4ddb9f9f3101119f466748c427080c8
David Calhoun <dpcalhoun@gmail.com>
2019-12-27 08:48:59 -0600

    Increase small screen text size

--- components/Paragraph.js
+++ components/Paragraph.js
@@ -4,7 +4,7 @@ import stripEmpty from "../utils/string";
 export default function Paragraph({ className, ...rest }) {
   return (
     <p
-      className={`text-md lg:text-2xl mb-4 lg:mb-8 ${stripEmpty(className)}`}
+      className={`text-lg lg:text-2xl mb-4 lg:mb-8 ${stripEmpty(className)}`}
       {...rest}
     />
   );
