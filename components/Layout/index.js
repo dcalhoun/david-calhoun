b1bff6c53df348320f6b5e5d464cd6d770d98a30
David Calhoun <dpcalhoun@gmail.com>
2019-12-27 10:49:38 -0600

    Add skip to content link

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -7,46 +7,53 @@ import TextButton from "../TextButton";
 
 export default function Layout({ children, disableContentPadding }) {
   return (
-    <div className="Layout min-h-screen p-2 lg:p-4 flex flex-col">
-      <div
-        className={`Layout-content ${
-          disableContentPadding ? "pt-8" : "px-4 pt-8"
-        } flex-1`}
-      >
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <nav
-          className={`text-center text-lg mb-8 ${
-            disableContentPadding ? "px-4" : ""
-          }`}
+    <>
+      <a className="SkipToContent" href="#content">
+        Skip to content
+      </a>
+      <div className="Layout min-h-screen p-2 lg:p-4 flex flex-col">
+        <div
+          className={`Layout-content ${
+            disableContentPadding ? "pt-8" : "px-4 pt-8"
+          } flex-1`}
         >
-          <Link href="/" passHref>
-            <TextButton className="mx-2">About</TextButton>
-          </Link>
-          <Link href="/writing" passHref>
-            <TextButton className="mx-2">Writing</TextButton>
-          </Link>
-          <TextButton
-            className="mx-2"
-            href="https://twitter.com/david_calhoun"
-            external
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+          </Head>
+          <nav
+            className={`text-center text-lg mb-8 ${
+              disableContentPadding ? "px-4" : ""
+            }`}
           >
-            Twitter
-          </TextButton>
-          <TextButton
-            className="mx-2"
-            href="https://github.com/dcalhoun"
-            external
-          >
-            GitHub
-          </TextButton>
-        </nav>
-        <div className="max-w-md lg:max-w-4xl m-auto">{children}</div>
+            <Link href="/" passHref>
+              <TextButton className="mx-2">About</TextButton>
+            </Link>
+            <Link href="/writing" passHref>
+              <TextButton className="mx-2">Writing</TextButton>
+            </Link>
+            <TextButton
+              className="mx-2"
+              href="https://twitter.com/david_calhoun"
+              external
+            >
+              Twitter
+            </TextButton>
+            <TextButton
+              className="mx-2"
+              href="https://github.com/dcalhoun"
+              external
+            >
+              GitHub
+            </TextButton>
+          </nav>
+          <main className="max-w-md lg:max-w-4xl m-auto" id="content">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
