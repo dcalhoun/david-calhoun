4c247cf9d8f4999940ae897b27890f04c76b29b5
David Calhoun <dpcalhoun@gmail.com>
2020-02-01 22:13:31 -0600

    Add custom definitions

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,13 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module "babel-plugin-preval/macro" {
+  declare function preval<T>(
+    template: TemplaTemplateStringsArray,
+    ...substitutions: any[]
+  ): T;
+
+  export = preval;
+}
+
+declare module "lazysizes";
