a18c34103fb88556092dbbe2aec24914cf3d24e9
David Calhoun <dpcalhoun@gmail.com>
2020-08-09 14:04:34 -0500

    Add App Reason component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,1 @@
-import App from "next/app";
-import Router from "next/router";
-
-Router.events.on("routeChangeComplete", (page_path) =>
-  window.gtag("config", process.env.GA_TRACKING_ID, { page_path })
-);
-
-export default App;
+export { make as default } from "./App.bs";
