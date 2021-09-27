135933a806f75450bb3d679b164250e093f7637a
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-09-26 20:00:33 -0400

    Generate Next.js site

--- components/Paragraph.res
+++
@@ -1,3 +0,0 @@
-@react.component
-let make = (~className="", ~children, ~style=?) =>
-  <p className={"font-serif text-base md:text-2xl mb-4 md:mb-8 " ++ className} ?style> children </p>
--- next.config.js
+++ next.config.js
@@ -1,24 +1,3 @@
-const bsconfig = require("./bsconfig.json");
-const withTM = require("next-transpile-modules")(
-  ["rescript"].concat(bsconfig["bs-dependencies"])
-);
-
-let withMDX = require("@next/mdx")();
-
-module.exports = withMDX(
-  withTM({
-    pageExtensions: ["mdx", "js", "bs.js"],
-    webpack: (config, options) => {
-      const { isServer } = options;
-      if (!isServer) {
-        // We shim fs for things like the blog slugs component
-        // where we need fs access in the server-side part
-        config.resolve.fallback = {
-          fs: false,
-          path: false,
-        };
-      }
-      return config;
-    },
-  })
-);
+module.exports = {
+  reactStrictMode: true,
+}
