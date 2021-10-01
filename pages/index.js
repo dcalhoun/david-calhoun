c695e44d91c92bbb0573f66d2345aa9b38c49633
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-10-01 16:41:03 -0400

    Update About content

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,11 @@ export default function Home() {
   return (
     <Layout>
       <main className="prose lg:prose-xl mx-auto px-2">
-        <h1>Hi. 👋🏻 I’m David Calhoun.</h1>
+        <h1>
+          Hi. 👋🏻
+          <br />
+          I’m David Calhoun.
+        </h1>
         <p>
           I am a {DESCRIPTION}. Currently, I am a Senior Software Engineer at{" "}
           <a href="https://automattic.com/">Automattic</a> building Gutenberg,
