6b33e82bac6306b418703538c92ca4a94879e86f
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-10-03 21:59:45 -0400

    Improve meta tags
    Use absolute URL for Open Graph image. Add Open Graph image size tags.
    Use `key` to ensure duplicative tags are handled.

--- components/layout-nextra.js
+++ components/layout-nextra.js
@@ -1,36 +1,82 @@
 import Head from "next/head";
 import PostList from "./post-list";
 import { DESCRIPTION } from "../components/layout";
+import { useRouter } from "next/router";
+
+const siteOrigin =
+  process.env.NODE_ENV === "production"
+    ? "https://davidcalhoun.me"
+    : "http://localhost:3000";
 
 export default function LayoutNextra({
   meta: {
-    title = "David Calhoun, Software Engineer",
     description = `David Calhoun is a ${DESCRIPTION}`,
+    image = siteOrigin + "/public/david.jpg",
+    imageAlt = "Portrait of David Calhoun",
+    imageHeight = 630,
+    imageWidth = 1200,
+    title = "David Calhoun, Software Engineer",
     type,
-    image = "/public/david.jpg",
   },
   pageMap,
 }) {
   return function LayoutBlog({ children }) {
+    const { asPath } = useRouter();
     return (
       <div className="animate-fade-up">
         <Head>
           <meta
+            key="viewport"
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <title>{title}</title>
-          <meta name="og:title" content={title} />
-          <meta name="description" content={description} />
-          <meta property="og:title" content={title} />
+          <title key="title">{title}</title>
+          <meta key="description" name="description" content={description} />
+          <meta key="og:title" property="og:title" content={title} />
           <meta
+            key="og:description"
+            property="og:description"
+            content={description}
+          />
+          <meta
+            key="og:site_name"
             property="og:site_name"
             content="David Calhoun, Software Engineer"
           />
-          <meta property="og:description" content={description} />
-          <meta property="og:image" content={image} />
-          <meta name="twitter:site" content="@david_calhoun" />
-          <meta name="twitter:card" content="summary_large_image" />
+          <meta key="og:image" property="og:image" content={image} />
+          <meta
+            key="og:image:height"
+            property="og:image:height"
+            content={imageHeight}
+          />
+          <meta
+            key="og:image:width"
+            property="og:image:width"
+            content={imageWidth}
+          />
+          <meta
+            key="og:image:alt"
+            property="og:image:width"
+            content={imageAlt}
+          />
+          <meta key="og:type" property="og:type" content="website" />
+          <meta key="og:url" property="og:url" content={siteOrigin + asPath} />
+          <meta
+            key="twitter:card"
+            name="twitter:card"
+            content="summary_large_image"
+          />
+          <meta
+            key="twitter:site"
+            name="twitter:site"
+            content="@david_calhoun"
+          />
+          <meta key="twitter:title" name="twitter:title" content={title} />
+          <meta
+            key="twitter:description"
+            name="twitter:description"
+            content={description}
+          />
         </Head>
         <div className="prose md:prose-lg lg:prose-2xl dark:prose-light mb-8 lg:mb-16">
           {children}
